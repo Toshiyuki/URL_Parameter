82455d95cb0427884760e808ae5653f7f7b5fbd7
ToshiyukiMIYAZAWA <toshi@nomade.biz>
2011-12-20 17:22:19 +0900

    alpha test

--- url_parameter.js
+++ url_parameter.js
@@ -1,1 +1,35 @@
 // JavaScript Document
+
+function URL_Parameter(url) {
+	this.url = url;
+	
+	var protocol = new Array();
+	
+	var g = /(https?|ftp):\/\//g;
+	
+  	if (url.match(g)) {
+		protocol = url.match(g);
+		this.url = url.replace(g,'');
+	}
+	
+	this.protocol = protocol;
+	
+	var result = this.url.split("/");
+	
+	
+	this.showProtocol = function(){
+		return protocol;
+	}
+	
+	//
+	this.showDomain = function(){
+		var domain = result[0];
+		return domain;
+	}
+	
+	//
+	this.showFile = function(){
+		var file = result[result.length - 1];
+		return file;
+	}
+}
